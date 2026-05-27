0f4ef6736121a2cfb2e8bec465c153ed13576f3a
lee castro <castroallanelee@gmail.com>
2026-05-27 11:40:44 +0800

    fix: nawawalang comments?

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import tkinter as tk
 
-
+# TODO: place the mouse script here
 def activate_script() -> None:
     pass
+    
 
-
+# simple ui that litrlly just contains the button cuz thats the only thing it needs to have :D
 def build_ui() -> None:
+
     root = tk.Tk()
     root.title("fons farm")
     root.geometry("420x240")
@@ -21,10 +23,11 @@ def build_ui() -> None:
     )
     title.pack(pady=(0, 24))
 
+    # the only part here that matters wc is the button
     activate_button = tk.Button(
         frame,
         text="ACTIVATE NOWWW!!!",
-        command=activate_script,
+        command=activate_script,  # call the script pag cinlick 
         font=("Arial", 14, "bold"),
         padx=18,
         pady=12,
